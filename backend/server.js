e3ae206ecb3c7dc7a075ca81c52a19c4de8e8a85
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-09 00:35:43 +0530

    - edited server js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
 dotenv.config()
 
-const app = express()
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+const app = express()
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
