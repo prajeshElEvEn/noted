9e31a4334c62abd586e96643c7cd9de7a98505a0
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-09 13:17:23 +0530

    styled nav footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import Github from '../assets/svg/github.svg'
 
 const Footer = () => {
     return (
-        <div className='footer'>
-            <div className='footer-left'>
-                &copy; {new Date().getFullYear()} | ElEvEn Co.
-            </div>
-            <div className='footer-right'>
-                <a href='' target='_blank' >
-                    Github
-                </a>
+        <div className='footer-container'>
+            <div className='footer'>
+                <div className='footer-left'>
+                    &copy; {new Date().getFullYear()} | ElEvEn Co.
+                </div>
+                <div className='footer-right'>
+                    <a href='https://github.com/prajeshElEvEn/noted' target='_blank' rel="noreferrer" >
+                        <img src={Github} alt='Github' />
+                        <span>
+                            Github
+                        </span>
+                    </a>
+                </div>
             </div>
         </div>
     )
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Login from '../assets/svg/login.svg'
+import Register from '../assets/svg/register.svg'
 
 const Header = () => {
     return (
-        <nav>
-            <div className="nav-left"></div>
-            <div className="nav-right">
-                <Link to={'/explore'} className='nav-item'>
-                    Explore
-                </Link>
-                <Link to={'/contact'} className='nav-item'>
-                    Contact Us
-                </Link>
-                <Link to={'/about'} className='nav-item'>
-                    Know More
-                </Link>
-                <Link to={'/login'} className='nav-btn-outline'>
-                    Login
-                </Link>
-                <Link to={'/register'} className='nav-btn'>
-                    Sign Up
-                </Link>
-            </div>
-        </nav>
+        <div className='nav-container'>
+            <nav>
+                <div className="nav-left"></div>
+                <div className="nav-right">
+                    <Link to={'/explore'} className='nav-item'>
+                        Explore
+                    </Link>
+                    <Link to={'/contact'} className='nav-item'>
+                        Contact Us
+                    </Link>
+                    <Link to={'/about'} className='nav-item'>
+                        Know More
+                    </Link>
+                    <Link to={'/login'} className='nav-btn-outline'>
+                        <span>
+                            Login
+                        </span>
+                        <img src={Login} alt='Login' />
+                    </Link>
+                    <Link to={'/register'} className='nav-btn'>
+                        <span>
+                            Sign Up
+                        </span>
+                        <img src={Register} alt='Register' />
+                    </Link>
+                </div>
+            </nav>
+        </div>
     )
 }
 
